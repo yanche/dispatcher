eb15e4cf7a7e9eba144e0ed9a9002072c2b00cf2
Yan Chen <yanche.belongs@gmail.com>
2017-07-16 21:01:30 -0700

    support getAll verb

--- src/dispatcher/handlers/get.ts
+++ src/dispatcher/handlers/get.ts
@@ -44,7 +44,7 @@ export async function getMul(ctx: Context<{ filter: Object; fields?: Object, ord
         if (!utility.validate.isObj(filter) || (!utility.validate.isObj(fields) && fields !== null) || (!utility.validate.isObj(orderby) && orderby !== null)
             || !utility.validate.pos(page, true) || !utility.validate.pos(pageSize, true) || pageSize > 30) {
             ctx.status = 400;
-            ctx.message = pageSize > 30 ? "maximum pagesize is 30" : "http request body has bad format, must be {filter:, fields:}";
+            ctx.message = pageSize > 30 ? "maximum pagesize is 30" : "http request body has bad format, must be {filter:, fields:, orderby:, page:, pageSize: }";
         }
         else {
             const _flt = utility.mongo.objIdfy(filter);
@@ -57,6 +57,30 @@ export async function getMul(ctx: Context<{ filter: Object; fields?: Object, ord
                 total: data[1],
                 page: page,
                 pageSize: pageSize
+            };
+            await next();
+        }
+    }
+}
+
+export async function getAll(ctx: Context<{ filter: Object; fields?: Object }>, next: () => any, colc: utility.mongo.CollClient<Task>) {
+    const body = ctx.request.body || { filter: {}, fields: null };
+    if (!utility.validate.isObj(body)) {
+        ctx.status = 400;
+        ctx.message = "http request body must be an object";
+    }
+    else {
+        const filter = body.filter || {};
+        const fields = body.fields || null;
+        if (!utility.validate.isObj(filter) || (!utility.validate.isObj(fields) && fields !== null)) {
+            ctx.status = 400;
+            ctx.message = "http request body has bad format, must be {filter:, fields:}";
+        }
+        else {
+            const _flt = utility.mongo.objIdfy(filter);
+            const data = await colc.getAll(_flt, fields);
+            ctx.body = {
+                list: data
             };
             await next();
         }
